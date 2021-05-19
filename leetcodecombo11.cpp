c85a9f0b3bdf705a878d77e6d0cbefa6ce56b9f5
Ashok <68994450+AshokDon@users.noreply.github.com>
2021-05-19 20:12:39 +0530

    Add files via upload

---
+++ leetcodecombo11.cpp
@@ -0,0 +1,30 @@
+    void printcombinations(vector<int>&candidates,int target,vector<int>&ans,vector<vector<int>>&mainans,int ind)
+    {
+        if(ind==candidates.size())
+        {
+            if(target==0)
+            {
+                mainans.push_back(ans);
+            }
+            return;
+        }
+        //logic 
+        if(candidates[ind]<=target)
+        {
+            ans.push_back(candidates[ind]);
+            printcombinations(candidates,target-candidates[ind],ans,mainans,ind+1);
+            ans.pop_back();
+        }
+        printcombinations(candidates,target,ans,mainans,ind+1);
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+        vector<int>ans;
+        vector<vector<int>>mainans;
+        printcombinations(candidates,target,ans,mainans,0);
+       
+        sort(mainans.begin(), mainans.end());
+        mainans.erase(unique(mainans.begin(), mainans.end()), mainans.end());
+        return mainans;
+    }
+};
