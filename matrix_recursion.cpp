d99e940a885651de16fb8a9345bb9f6d27e2c744
Ashok <68994450+AshokDon@users.noreply.github.com>
2021-08-21 14:36:05 +0530

    Add files via upload

--- matrix_recursion.cpp
+++ matrix_recursion.cpp
@@ -1,38 +1,33 @@
 #include <iostream>
 #include<bits/stdc++.h>
 using namespace std;
-int n=3;
-int m=3;
-int arr[3][3]={{1,1,0},{1,1,1},{1,0,1}};
-void printpaths(int i,int j,vector<pair<int,int>>&paths)
+void FindPaths(int i,int j,int n,int m,int *mat,string s)
+
 {
-    //check for invalid paths and stop going
-    if(i>=n || j>=m || arr[i][j]==0)
+    if(i>=n || j>=m)
     {
         return;
     }
     if(i==n-1 && j==m-1)
     {
-        cout<<"0 0->";
-        for(auto it:paths)
-        {
-            cout<<it.first<<" "<<it.second<<"->";
-        }
-        cout<<n-1<<" "<<m-1<<endl;
+        cout<<s<<"\n";
+        return;
     }
-    //move down
-    paths.push_back({i+1,j});
-    printpaths(i+1,j,paths);
-    paths.pop_back();
-
-    //move right
-    paths.push_back({i,j+1});
-    printpaths(i,j+1,paths);
-    paths.pop_back();
+    s+="D";
+    FindPaths(i+1,j,n,m,mat,s);
+    s.pop_back();
+    
+    s+="R";
+    FindPaths(i,j+1,n,m,mat,s);
+    s.pop_back();
 }
 
 int main() {
-    vector<pair<int,int>>paths;
-    printpaths(0,0,paths);
+	int n,m;
+	cin>>n>>m;
+	int mat[n][m];
+	memset(mat,0,sizeof mat);
+	string s="";
+	FindPaths(0,0,n,m,*mat,s);
 	return 0;
 }
